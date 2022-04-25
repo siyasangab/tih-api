d653e54e0ca7989cf76e5ebc4d076008cd44ad46
STRUCTUREIT\siyasanga.busakwe <siyasanga.busakwe@structureit.net>
2022-04-25 16:18:53 +0200

    Add inmemory and browser caching

---
+++ GitAssessment.Services/Caching/CacheService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitAssessment.Services.Caching
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IDistributedCache distributedCache;
+
+        public CacheService(IDistributedCache distributedCache)
+        {
+            this.distributedCache = distributedCache;
+        }
+
+        public async Task<T> Get<T>(string key)
+        {
+            var value = await distributedCache.GetStringAsync(key);
+
+            if (value != null)
+            {
+                return JsonConvert.DeserializeObject<T>(value);
+            }
+
+            return default;
+        }
+
+        public async Task<T> Set<T>(string key, T value)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1),
+                SlidingExpiration = TimeSpan.FromMinutes(5)
+            };
+
+            await distributedCache.SetStringAsync(key, JsonConvert.SerializeObject(value), options);
+
+            return value;
+        }
+    }
+}
---
+++ GitAssessment.Services/Caching/ICacheService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitAssessment.Services.Caching
+{
+    public interface ICacheService
+    {
+        Task<T> Get<T>(string key);
+        Task<T> Set<T>(string key, T value);
+    }
+}
