2140f441c90f817bb9537e700605a46e8b0320f1
STRUCTUREIT\siyasanga.busakwe <siyasanga.busakwe@structureit.net>
2022-04-25 11:13:20 +0200

    Add word-types endpoint

---
+++ GitAssessment.Api/Controllers/WordTypesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitAssement.Api.Controllers
+{
+    [Route("api/word-types")]
+    [ApiController]
+    public class WordTypesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            return Ok();
+        }
+    }
+}
