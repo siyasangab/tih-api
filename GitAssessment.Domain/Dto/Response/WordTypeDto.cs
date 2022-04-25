c64bc436fe41228576818ac725554dac307124d3
STRUCTUREIT\siyasanga.busakwe <siyasanga.busakwe@structureit.net>
2022-04-25 11:25:13 +0200

    Add word type dto

---
+++ GitAssessment.Domain/Dto/Response/WordTypeDto.cs
@@ -0,0 +1,15 @@
+﻿using GitAssessment.Domain.Entities;
+
+namespace GitAssement.Domain.Dto.Response
+{
+    public class WordTypeDto
+    {
+        public WordTypeDto(WordType wordType)
+        {
+            Id = wordType.Id;
+            Name = wordType.Name;
+        }
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
